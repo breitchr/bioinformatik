f97c67e868c33dc5d38a00f9015f2a1b3336d921
Christian Breitenberger <christian.breitenberger@gmail.com>
2016-04-30 11:57:50 +0200

    HandlingPDB.cpp: Rewrite of coordinate selection, adding offset and replacing the new value

--- exercise1/HandlingPDB.cpp
+++ exercise1/HandlingPDB.cpp
@@ -10,23 +10,24 @@ int main(int argc, char* argv[]) {
 
 
   std::string line;
-  std::ifstream pdbfile
-  std::ofstream translatefile
+  std::ifstream pdbfile;
+  std::ofstream translatefile;
   std::vector<double> d;
 
   if(argc == 3){
 
     pdbfile.open(argv[1], std::ios::in);
     translatefile.open(argv[2], std::ios::out);
-    d.assign(3,1);
+    d.assign(3,1.000);
 
   }else if(argc == 6){
 
     pdbfile.open(argv[1], std::ios::in);
     translatefile.open(argv[2], std::ios::out);
-    d.push_back(stof(argv[3]));
-    d.push_back(stof(argv[4]));
-    d.push_back(stof(argv[5]));
+  
+    d.push_back(stod(argv[3]));
+    d.push_back(stod(argv[4]));
+    d.push_back(stod(argv[5]));
 
   }else{
 
@@ -34,21 +35,71 @@ int main(int argc, char* argv[]) {
     return 1;
 
   }
-    
+
+  //Basic idea:
+  // Checking ATOM
+  // Grabbing Coordinates based on columns
+  // Parsing to double, adding offset
+  // And the c++ way to parsing double to string with fixed size and precision is to set up an outsream correctly
+  // Important: If you want a precision of 3, i.e 3 digits after the dot, you need an outstream.precision of 4. Otherwise
+  // it get rounded to early.
+  // Lastly, replacing the new string in the selected line
   if (pdbfile.is_open()){
     while (getline(pdbfile,line)){
       
-      std::string buf;
-      std::stringstream ss(line);
-      std::vector<std::string> items;
+      //std::string buf;
+      //std::stringstream ss(line);
+      //std::vector<std::string> items;
       
+      /*
       while (ss >> buf){
         items.push_back(buf);
+      }*/
+      
+      //std::vector<int>::size_type sz = items.size();
+      
+      if (line.substr(0,4) == "ATOM"){
+
+        double xCoord = stod(line.substr(30,8));
+        double yCoord = stod(line.substr(38,8));
+        double zCoord = stod(line.substr(46,8));
+
+
+        xCoord += d[0];
+        yCoord += d[1];
+        zCoord += d[2];
+
+        std::ostringstream xCoordStrs;
+        std::ostringstream yCoordStrs;
+        std::ostringstream zCoordStrs;
+
+        xCoordStrs.width(8);
+        xCoordStrs.precision(4);
+        yCoordStrs.width(8);
+        yCoordStrs.precision(4);
+        zCoordStrs.width(8);
+        zCoordStrs.precision(4);
+
+        xCoordStrs << xCoord;
+        yCoordStrs << yCoord;
+        zCoordStrs << zCoord;
+
+        line.replace(30,8,xCoordStrs.str());
+        line.replace(38,8,yCoordStrs.str());
+        line.replace(46,8,zCoordStrs.str());
       }
-      
-      std::vector<int>::size_type sz = items.size();
-      
-      if (items[0] == "ATOM"){
+
+      translatefile << line << '\n';
+
+    }
+
+  }else{
+
+    std::cout << "Unable to open file";
+
+  }
+      /*
+
 	for (unsigned i = 0; i < sz; i++){
 	  // positions 6,7,8 contain x,y,z for an atom line in a pdb file
 	  if (i == 6){
@@ -62,7 +113,7 @@ int main(int argc, char* argv[]) {
 	    translatefile << stof(items[i]) + d[2] << ' ';
 	  }
 	  else{
-	    translatefile << items[i] << ' ';
+	    translatefile << line;
 	  }
 	}
 	translatefile << std::endl;
@@ -84,6 +135,7 @@ int main(int argc, char* argv[]) {
   }
 
   else std::cout << "Unable to open file"; 
+*/
 
   return 0;
 }
