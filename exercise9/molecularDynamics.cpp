fa723d9c81fa213aa4eae499a9565cf399839a08
Janine Nicodemus <jnicodem@students.uni-mainz.de>
2016-06-25 05:42:25 -0700

    working version: pdb download and read in system

--- exercise9/molecularDynamics.cpp
+++ exercise9/molecularDynamics.cpp
@@ -10,23 +10,23 @@ int main(int argc, char* argv[]){
   System mdSystem;
   string url = "http://www.rcsb.org/pdb/files/";
   string pdbid;
+
   //Sanity checks for command-line arguments	
-  if(argc == 3){
+  if(argc == 2){
     pdbid.append(argv[1] + string(".pdb"));
-    s.append(pdbid);
+    url.append(pdbid);
   }else{
     cout << "Wrong amount of Parameters\n\n Useage: prog pdbid" << endl;
     return 1;
   }
 
-  system("wget ");
-  sourceFile.open(pdbid + string(".pdb"));
+  system((string("wget ") + url).c_str());
+  sourceFile.open(pdbid);
   if(sourceFile.is_open()){
     sourceFile.read(mdSystem);
     sourceFile.close();
   }
 
-  
-
+ 
   return 0;
 }
